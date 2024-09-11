5b008d325152dad23b38de31b3a71d4e95903754
cloudchamb3r <jizon0123@protonmail.com>
2024-09-11 12:30:50 +0900

    filter binary files

--- routers/web/repo/highlight_download.go
+++ routers/web/repo/highlight_download.go
@@ -3,6 +3,7 @@ package repo
 import (
 	"io"
 	"net/http"
+	"unicode/utf8"
 
 	"html/template"
 
@@ -10,6 +11,11 @@ import (
 	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/services/context"
 )
+
+type HighlightResponse struct {
+	Html  []template.HTML `json:"html"`
+	IsBin bool            `json:"isBin"`
+}
 
 func SingleHighlightDownload(ctx *context.Context) {
 	blob, _ := getBlobForEntry(ctx)
@@ -32,16 +38,18 @@ func SingleHighlightDownload(ctx *context.Context) {
 		ctx.JSONErrorf("error on io.readall : %v", err)
 		return
 	}
+
+	isText := utf8.ValidString(string(rawCode))
+	if !isText {
+		ctx.JSON(http.StatusOK, HighlightResponse{IsBin: true})
+		return
+	}
+
 	htmlCode, _, err := highlight.File(blob.Name(), "", rawCode)
 	if err != nil {
 		ctx.JSONErrorf("error on highlight %v", err)
 		return
 	}
 
-	response := struct {
-		Html []template.HTML `json:"html"`
-	}{
-		Html: htmlCode,
-	}
-	ctx.JSON(http.StatusOK, response)
+	ctx.JSON(http.StatusOK, HighlightResponse{Html: htmlCode, IsBin: false})
 }
